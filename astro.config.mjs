b55fa5c558f40a57585447ca78e31a5f03df163f
j329nish <j520329b@mails.cc.ehime-u.ac.jp>
2025-06-08 16:56:57 +0900

    Add code for deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://j329nish.github.io',
+  base: 'Portfolio',
   vite: {
       plugins: [tailwindcss()],
   },
