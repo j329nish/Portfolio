51fecd5fe12a65429a2153b9b60da01e83191e11
j329nish <j520329b@mails.cc.ehime-u.ac.jp>
2025-06-08 17:00:06 +0900

    Fixed astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://j329nish.github.io',
-  base: 'Portfolio',
+  // base: 'Portfolio',
   vite: {
       plugins: [tailwindcss()],
   },
